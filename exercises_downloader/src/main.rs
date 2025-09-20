d33c80d3113cc19923aadccefb5bfb09d5557593
markgreene74 <markgreene74@users.noreply.github.com>
2025-09-20 18:49:38 +0100

    first stab, file writing functions
    ... and error propagration

--- exercises_downloader/src/main.rs
+++ exercises_downloader/src/main.rs
@@ -5,6 +5,8 @@
 use serde::Deserialize;
 use std::env;
 use std::fs;
+use std::fs::File;
+use std::io::Write;
 use std::path::Path;
 
 #[derive(Deserialize, Debug)]
@@ -18,22 +20,46 @@ struct Bite {
     author: String,
 }
 
-fn write_root_toml(base_path: &Path) {
-    dbg!(base_path.join("Cargo.toml"));
+fn write_root_toml(path: &Path) -> std::io::Result<()> {
+    // main Cargo.toml template
+    let content = b"[package]
+name = \"exercises\"
+version = \"0.1.0\"
+edition = \"2024\"\
+\n
+[workspace]
+resolver = \"3\"
+members = [\"adder\"]";
+
+    let filename = path.join("Cargo.toml");
+    let mut file = File::create(filename)?;
+    file.write_all(content)?;
+    Ok(())
 }
 
-fn write_toml(slug: &String, libraries: String) {
+fn write_toml(path: &Path, slug: &String, libraries: String) -> std::io::Result<()> {
+    dbg!(&path);
     dbg!(&slug);
     dbg!(&libraries);
+    Ok(())
 }
 
-fn write_exercise(template: String) {
+fn write_exercise(path: &Path, template: String) -> std::io::Result<()> {
+    dbg!(&path);
     dbg!(&template);
+    Ok(())
 }
-fn write_markdown(slug: &String, description: String, author: String) {
+fn write_markdown(
+    path: &Path,
+    slug: &String,
+    description: String,
+    author: String,
+) -> std::io::Result<()> {
+    dbg!(&path);
     dbg!(&slug);
     dbg!(&description);
     dbg!(&author);
+    Ok(())
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -73,16 +99,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         // make sure the exercise directory exists
         fs::create_dir_all(&exercise_path)?;
-        // re-write/update the toml and md files but make  a backup copy of the
+        // re-write/update the toml and md files but make a backup copy of the
         // exercise file if it exists, in case it was already solved
-        write_toml(&slug, bite.libraries);
-        write_markdown(&slug, bite.description, bite.author);
-        write_exercise(bite.template);
-        // TODO add error propagation ^^^
+        write_toml(&exercise_path, &slug, bite.libraries)?;
+        write_markdown(&exercise_path, &slug, bite.description, bite.author)?;
+        write_exercise(&exercise_path, bite.template)?;
         println!(" ✅");
     }
-    write_root_toml(&base_path);
-    // TODO add error propagation ^^^
+
+    write_root_toml(&base_path)?;
 
     // all done
     Ok(())
