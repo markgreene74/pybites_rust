157449f4686f6ca5d5c20f6f621ab11c963cb922
markgreene74 <markgreene74@users.noreply.github.com>
2025-09-20 14:06:52 +0100

    simpla parsing of the json response

--- exercises_downloader/src/main.rs
+++ exercises_downloader/src/main.rs
@@ -1,25 +1,46 @@
 // Download the exercises from https://rustplatform.com/
 // and make them available locally.
 
-use anyhow::Result;
+use reqwest::Error;
+use serde::Deserialize;
 use std::env;
 
-fn main() -> Result<()> {
+#[derive(Deserialize, Debug)]
+struct Bite {
+    name: String,
+    slug: String,
+    description: String,
+    level: String,
+    template: String,
+    libraries: String,
+    author: String,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Download the exercises from Pybites Rust");
 
     // collect arguments
     let args: Vec<String> = env::args().collect();
 
     // send the request
-    let response = reqwest::blocking::get("https://rustplatform.com/api/")?;
+    let client = reqwest::blocking::Client::new();
+    let response = client.get("https://rustplatform.com/api/").send()?;
+    println!("Status: {}", response.status()); // DEBUG
+    println!();
 
     // just testing, print out the status and headers and exit
     if args.contains(&String::from("--test")) {
-        println!("Status: {}", response.status());
         println!("Headers:\n{:#?}", response.headers());
         return Ok(());
     }
-    // println!("Body:\n{}", body);
-    println!("Status: {}", response.status());
+
+    // extract the exercises from the response
+    let bites: Vec<Bite> = response.json()?;
+    println!("I found {:#?} exercises!", bites.len());
+    println!();
+
+    for bite in bites {
+        println!("{:#?}", bite.name);
+    }
     Ok(())
 }
